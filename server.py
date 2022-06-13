e124f91eff7f01a4522e6ec0bc0bda7343e8cdb1
Ivan Gerov <igerov11@gmail.com>
2022-06-13 16:41:47 +0200

    Initial commit

---
+++ server.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+
+HOST = "192.168.1.103"
+PORT = 8000
+DIRECTORY = "books"
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+
+with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
+    print(f"Serving books at {HOST}:{PORT}")
+    httpd.serve_forever()
